7d23487c0abdda14844c65371c51713ed11997fa
Zainab Salih <Zainab.Salih@outlook.com>
2025-05-17 01:05:38 +0400

    so_long: get_map.ber

--- get_map.c
+++ get_map.c
@@ -6,7 +6,7 @@
 /*   By: zsalih < zsalih@student.42abudhabi.ae>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 11:45:48 by zsalih            #+#    #+#             */
-/*   Updated: 2025/05/13 12:09:28 by zsalih           ###   ########.fr       */
+/*   Updated: 2025/05/17 01:04:52 by zsalih           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,58 @@ int	has_ber_extension(const char *filename)
 	return (strncmp(filename + (len - 4), ".ber") == 0);
 }
 
-void	get_map(const char *filename)
+static t_list	*read_map(int fd)
 {
-	int fd;
-	char *line;
+	t_list	*map_list;
+	char	*line;
 
-	fd = open(filename, O_RDONLY);	
+	map_list = NULL;
+	line = get_next_line(fd);
+	while (line)
+	{
+		ft_lstadd_back(&map_list, ft_lstnew(line));
+		line = get_next_line(fd);
+	}
+	return (map_list);
+}
+
+static char	**list_to_array(t_list *lst, int height)
+{
+	char	**map;
+	int		i;
+
+	i = 0;
+	map = ft_calloc(height + 1, sizeof(char *));
+	if (!map)
+		return (NULL);
+	while (lst)
+	{
+		map[i++] = lst->content;
+		lst = lst->next;
+	}
+	map[i] = NULL;
+	return (map);
+}
+
+char	**get_map(const char *filename)
+{
+	int		fd;
+	t_list	*map_list;
+	char	**map;
+	int		height;
+
+	map_list = NULL;
+	height = 0;
+	if (!has_ber_extension(filename))
+		exit(EXIT_FAILURE);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		exit(EXIT_FAILURE);
+	map_list = read_map(fd);
+	close(fd);
+	height = ft_lstsize(map_list);
+	map = list_to_array(map_list, height);
+	ft_lstclear(&map_list, NULL);
+	check_map_validity(map);
+	return (map);
 }
--- so_long.c
+++ so_long.c
@@ -6,21 +6,24 @@
 /*   By: zsalih < zsalih@student.42abudhabi.ae>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 13:23:59 by zsalih            #+#    #+#             */
-/*   Updated: 2025/05/13 12:01:25 by zsalih           ###   ########.fr       */
+/*   Updated: 2025/05/17 01:05:12 by zsalih           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    if (ac > 1)
-    {
-        if (!has_ber_extension(av[1]))
-        {
-            ft_putendl_fd("Error: Invalid file extension. Use .ber files only.", 2);
-            exit(EXIT_FAILURE);
-        }
-        get_map(av[1]);
-    }
+	char	**map;
+
+	if (ac > 1)
+	{
+		if (!has_ber_extension(av[1]))
+		{
+			ft_putendl_fd("Error: Invalid file extension. Use .ber files only.",
+				2);
+			exit(EXIT_FAILURE);
+		}
+		map = get_map(av[1]);
+	}
 }
